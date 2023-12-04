51bcf78e0b46d97ec1bc40d5ca737f51408030e2
savaleriy <semenyuk_va@tu.tusur.ru>
2023-12-04 17:04:33 +0700

    using threading

--- client.py
+++ client.py
@@ -1,15 +1,36 @@
+# client.py
+
 import socket
-import encryption  # Импортируем модуль для шифрования и дешифрования сообщений
+import threading
 
-# Создаем сокет клиента
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('127.0.0.1', 12345)
+def receive_messages(s):
+    while True:
+        received = s.recv(1024).decode('utf-8')
+        if not received:
+            break
+        print(received)
 
-while True:
-    message = input("Введите сообщение: ")
+def send_messages(s):
+    while True:
+        user_input = input("$: ")
+        if user_input.strip() == 'exit':
+            s.sendall(user_input.encode('utf-8'))
+            break
+        elif user_input.strip():
+            s.sendall(user_input.encode('utf-8'))
 
-    # Шифруем сообщение с использованием шифра Цезаря перед отправкой
-    encrypted_message = encryption.encrypt_message(message, encryption.caesar_cipher, shift=3)  # Используем сдвиг 3 как пример
-    client.send(encrypted_message.encode())
+if __name__ == '__main__':
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.connect(('127.0.0.0', 8888))
 
-client.close()
+    receive_thread = threading.Thread(target=receive_messages, args=(s,))
+    send_thread = threading.Thread(target=send_messages, args=(s,))
+
+    receive_thread.start()
+    send_thread.start()
+
+    receive_thread.join()
+    send_thread.join()
+
+    s.close()
