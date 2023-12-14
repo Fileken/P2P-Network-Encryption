720ab500b4be971536cd4479950a706349a9c423
karasik227 <149232008+karasik227@users.noreply.github.com>
2023-12-14 12:13:22 +0700

    Update encryption.py

--- encryption.py
+++ encryption.py
@@ -1,3 +1,9 @@
+import random
+from re import findall
+
+def regular(text):
+    return findall(r"[0-9]+", text)
+
 def caesar_cipher(text, shift):
     result = ""
     for char in text:
@@ -15,20 +21,48 @@ def vinzher_cipher(text, key, mode):
     mode=mode.upper()
     key *= len(text) // len(key) + 1
     for index, symbol in enumerate(text):
-        if mode == 'e':
+        if mode == 'E':
             temp = ord(symbol) + ord(key[index])
         else:
             temp = ord(symbol) - ord(key[index])
         result += chr(temp % 26 + ord('A'))
     return result
 
-
+def xor_cipher(text, mode):
+    result = []
+    keys=[]
+    if mode == "E":
+        for symbol in text:
+            key=random.randint(0,25)
+            keys.append(str(key))
+            result.append(str(ord(symbol)^key))
+        return '.'.join(result), '.'.join(keys)
+    else:
+        keys=input("Write the keys: ")
+        for index, symbol in enumerate(regular(text)):
+            result.append(chr(int(symbol) ^ int(regular(keys)[index])))
+        return ''.join(result)
 
 def encrypt_message(message, encryption_function, *args):
+    
+    if encryption_function==vinzher_cipher:
+        args=list(args)
+        args.append("E")
+    if encryption_function==xor_cipher:
+        args=list(args)
+        args.append("E")
+        
     return encryption_function(message, *args)
 
 def decrypt_message(encrypted_message, decryption_function, *args):
+    
     if decryption_function==caesar_cipher:
         args=list(args)
         args[0]=-args[0]
+    if decryption_function==vinzher_cipher:
+        args=list(args)
+        args.append("D")
+    if decryption_function==xor_cipher:
+        args=list(args)
+        args.append("D")
     return decryption_function(encrypted_message, *args)
